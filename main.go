99fab05f24cb382caca770cf0cb17be9db8c21bf
Stefan Heiden <stefanheiden@gmail.com>
2024-06-08 08:54:43 +0200

    refactoring

--- main.go
+++ main.go
@@ -6,29 +6,42 @@ import (
     "os"
 )
 
-func main() {
-    file, err := os.Open("assets/keyboard.txt")
+
+func readAssest(path string) (string, error) {
+    file, err := os.Open(path)
     if err != nil {
-        log.Fatal(err)
+      return "", nil
     }
     defer file.Close()
-
     data := make([]byte, 1024) 
     count, err := file.Read(data)
     if err != nil {
-        log.Fatal(err)
+      return "", nil
     }
+    return string(data[:count]), nil
+}
 
-    htmlContent := fmt.Sprintf("<pre>%s</pre>", string(data[:count]))
+func writeHtml(path string, content string) error {
+    htmlContent := fmt.Sprintf("<pre>%s</pre>", content)
 
-    htmlFile, err := os.Create("ascii_art.html")
+    htmlFile, err := os.Create(path)
     if err != nil {
-        log.Fatal(err)
+      return err
     }
     defer htmlFile.Close()
 
     _, err = htmlFile.WriteString(htmlContent)
     if err != nil {
-        log.Fatal(err)
+      return err
     }
+    return nil
+}
+
+func main() {
+    keyboard, err := readAssest("assets/keyboard.txt")
+    if err != nil {
+      log.Printf("%s", err)
+      os.Exit(1)
+    }
+    writeHtml("keyboard.html", keyboard) 
 }
