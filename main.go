611f0ab981de366034f7ac6c71d5e8528dbe4626
Stefan Heiden <stefanheiden@gmail.com>
2024-06-08 09:05:14 +0200

    cal days since and render keyboard with them

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "log"
     "os"
+    "time"
 )
 
 
@@ -37,11 +38,25 @@ func writeHtml(path string, content string) error {
     return nil
 }
 
+func prepareKeyboard(keyboard string, value int) string  {
+  return fmt.Sprintf(keyboard, value, 2, 3)
+}
+
+func daysSinceGivenDate(year int, month time.Month, day int) int {
+    givenDate := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+    currentDate := time.Now()
+
+    days := int(currentDate.Sub(givenDate).Hours() / 24)
+    return days
+}
+
 func main() {
     keyboard, err := readAssest("assets/keyboard.txt")
     if err != nil {
       log.Printf("%s", err)
       os.Exit(1)
     }
+    daysSince := daysSinceGivenDate(2023, 9, 2)
+    keyboard = prepareKeyboard(keyboard, daysSince)
     writeHtml("keyboard.html", keyboard) 
 }
