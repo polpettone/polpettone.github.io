6214fb51e4ed76d6baf99bd6e452b75ab8eb5d25
Stefan Heiden <stefanheiden@gmail.com>
2024-07-05 23:07:24 +0200

    template html

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"time"
+  "os"
+  "html/template"
+  "strconv"
 )
 
 func calcDays(from, to time.Time) int {
@@ -12,13 +15,48 @@ func calcDays(from, to time.Time) int {
   return days
 }
 
+func render(days string) {
+    templateFile := "template.html"
+    tmplContent, err := os.ReadFile(templateFile)
+    if err != nil {
+        panic(err)
+    }
+
+    data := struct {
+        D1 string
+        D2 string
+        D3 string
+    }{
+        D1: string(days[0]),
+        D2: string(days[1]),
+        D3: string(days[2]),
+    }
+
+    t := template.Must(template.New("htmlTemplate").Parse(string(tmplContent)))
+
+    file, err := os.Create("index.html")
+
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+
+    err = t.Execute(file, data)
+    if err != nil {
+        panic(err)
+    }
+}
+
+
 func main() {
-  from, err := time.Parse("2006-01-02", "2023-09-02")
+  from, err := time.Parse("2006-01-02", "2022-09-02")
   if err != nil {
     log.Fatal(err)
   }
   days := calcDays(from, time.Now())
 
   fmt.Printf("Days: %d", days)
+
+  render(strconv.Itoa(days))
 }
 
