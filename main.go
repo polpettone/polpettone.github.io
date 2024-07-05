94d8015454f2b56ce64cd899b3e453a37b1704d0
Stefan Heiden <stefanheiden@gmail.com>
2024-07-05 22:42:05 +0200

    calc days

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+func calcDays(from, to time.Time) int {
+  duration := to.Sub(from)
+  days := int(duration.Hours()/24)
+  return days
+}
 
 func main() {
-  fmt.Println("Calculate Days")
+  from, err := time.Parse("2006-01-02", "2023-09-02")
+  if err != nil {
+    log.Fatal(err)
+  }
+  days := calcDays(from, time.Now())
+
+  fmt.Printf("Days: %d", days)
 }
+
