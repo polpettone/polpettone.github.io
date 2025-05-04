8a39f397de994689dbcf71b4475908df7756bd8c
Stefan Heiden <stefanheiden@gmail.com>
2025-05-04 10:54:16 +0200

    read run data from smashrun api

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
-	"log"
+	"io"
+	"net/http"
 	"os"
 	"strconv"
 	"time"
@@ -81,15 +83,84 @@ func render_kilometer(kilometers string) {
 	}
 }
 
-func main() {
-	from, err := time.Parse("2006-01-02", "2023-09-02")
+func fetchAPIData(url string) (string, error) {
+	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
-	days := calcDays(from, time.Now())
+	defer resp.Body.Close()
 
-	fmt.Printf("Days: %d", days)
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to fetch data: %s", resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+func fetchRunDataFromSmashrun(smashrunURL string, smashrunToken string) (*RunningStats, error) {
+	fetchUrl := fmt.Sprintf("%s?access_token=%s", smashrunURL, smashrunToken)
+	rawData, err := fetchAPIData(fetchUrl)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var stats RunningStats
+	err = json.Unmarshal([]byte(rawData), &stats)
+	if err != nil {
+		return nil, err
+	}
+
+	return &stats, nil
+}
+
+type RunningStats struct {
+	RunCount                       int      `json:"runCount"`
+	TotalDistance                  float64  `json:"totalDistance"`
+	AverageSpeed                   float64  `json:"averageSpeed"`
+	AveragePace                    string   `json:"averagePace"`
+	LongestRun                     float64  `json:"longestRun"`
+	LongestRunWhen                 string   `json:"longestRunWhen"`
+	LongestBreakBetweenRuns        int      `json:"longestBreakBetweenRuns"`
+	LongestBreakBetweenRunsDate    string   `json:"longestBreakBetweenRunsDate"`
+	LongestStreak                  int      `json:"longestStreak"`
+	LongestStreakDate              string   `json:"longestStreakDate"`
+	AverageDistancePerDay          *float64 `json:"averageDistancePerDay"`
+	AverageRunLength               float64  `json:"averageRunLength"`
+	AverageDaysRunPerWeek          float64  `json:"averageDaysRunPerWeek"`
+	MostOftenRunOnDay              string   `json:"mostOftenRunOnDay"`
+	MostOftenRunOnCount            int      `json:"mostOftenRunOnCount"`
+	MostOftenRunOnAverageDistance  float64  `json:"mostOftenRunOnAverageDistance"`
+	LeastOftenRunOnDay             string   `json:"leastOftenRunOnDay"`
+	LeastOftenRunOnCount           int      `json:"leastOftenRunOnCount"`
+	LeastOftenRunOnAverageDistance float64  `json:"leastOftenRunOnAverageDistance"`
+	DaysRunAM                      int      `json:"daysRunAM"`
+	DaysRunPM                      int      `json:"daysRunPM"`
+	DaysRunBoth                    int      `json:"daysRunBoth"`
+}
+
+func main() {
+
+	apiToken := os.Getenv("SMASHRUN_API_TOKEN")
+
+	runningStats, err := fetchRunDataFromSmashrun("https://api.smashrun.com/v1/my/stats", apiToken)
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	days := runningStats.LongestStreak
+	totalDistance := int(runningStats.TotalDistance)
+	totalDistanceStr := strconv.Itoa(totalDistance)
+
+	fmt.Printf("%d \n", totalDistance)
+	fmt.Printf("%d \n", days)
 
 	render_days(strconv.Itoa(days))
-	render_kilometer("3830")
+	render_kilometer(totalDistanceStr)
 }
